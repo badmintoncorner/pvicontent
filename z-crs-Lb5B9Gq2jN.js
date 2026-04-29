3623f6db63420930ebad3133af3ed0f571cc01be
a6Qoh59xf1bBRVU <172883705+a6Qoh59xf1bBRVU@users.noreply.github.com>
2026-04-29 14:15:29 +0800

    Update z-crs-Lb5B9Gq2jN.js

--- z-crs-Lb5B9Gq2jN.js
+++ z-crs-Lb5B9Gq2jN.js
@@ -58,14 +58,12 @@ document.addEventListener("DOMContentLoaded", () => {
   <use href="#menu-icon"></use>
 </svg> Expense</h6></th><th class="mdl nbrd w30"><h6 class="k6 j2"><svg width="15" height="15" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icn" viewBox="0 0 24 24">
   <line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3.01" y2="6"></line><line x1="3" y1="12" x2="3.01" y2="12"></line><line x1="3" y1="18" x2="3.01" y2="18"></line>
-</svg> Description</h6></th><th class="mdl nbrd w20"><h6 class="k6 j2"><svg width="15" height="15" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="icn" viewBox="0 0 24 24">
-  <use href="calendar-icon"></use>
-</svg> Date</h6></th><th class="mdl nbrd w20"><h6 class="k6 j2">Amount</h6></th></tr>
+</svg> Description</h6></th><th class="mdl nbrd w20"><h6 class="k6 j2">Date</h6></th><th class="mdl nbrd w20"><h6 class="k6 j2">Amount</h6></th></tr>
 <tr>
 <th class="mdl m-l w30"><p class="k9 j12">Transportation<br>Mr. Donut<br>Woonmart<br>National Bookstore</p></th>
 <th class="mdl w30"></th>
 <th class="mdl w20"><p class="k9 j12">April 29, 2026</p></th>
-<th class="mdl w20 m-r"><p class="k9 j12">₱ 33.00<br>₱ 60.00<br>₱ 95.00<br>₱ 39.00<br><b>₱ 227.00</b></p></th>
+<th class="mdl w20 m-r"><p class="k9 j12">₱ 33.00<br>₱ 60.00<br>₱ 95.00<br>₱ 39.00<hr><b>₱ 227.00</b></p></th>
 </tr>
 </tbody>
 </table>
