d7edaca833e5ec6aa21548ecbb4339f5dbcd622c
a6Qoh59xf1bBRVU <172883705+a6Qoh59xf1bBRVU@users.noreply.github.com>
2026-08-02 20:39:53 +0800

    Update uEC1mfc6mBHrl0LYEYD8fE7ILBHjxb67.js

--- uEC1mfc6mBHrl0LYEYD8fE7ILBHjxb67.js
+++ uEC1mfc6mBHrl0LYEYD8fE7ILBHjxb67.js
@@ -35,7 +35,51 @@ search.addEventListener("input", function () {
 
 // SAMPLE
 
+function clearAttendance() {
+    const ids = [
+        "acarEA","acarET","acarA","acarT","acarMP",
+        "atienzaEA","atienzaET","atienzaA","atienzaT","atienzaMP",
+        "bacallaEA","bacallaET","bacallaA","bacallaT","bacallaMP",
+        "banaagEA","banaagET","banaagA","banaagT","banaagMP",
+        "barzaEA","barzaET","barzaA","barzaT","barzaMP",
+        "boncalosEA","boncalosET","boncalosA","boncalosT","boncalosMP",
+        "catapangJMEA","catapangJMET","catapangJMA","catapangJMT","catapangJMMP",
+        "decastroGVEA","decastroGVET","decastroGVA","decastroGVT","decastroGVMP",
+        "decastroFLEA","decastroFLET","decastroFLA","decastroFLT","decastroFLMP",
+        "deleonEA","deleonET","deleonA","deleonT","deleonMP",
+        "fontillasEA","fontillasET","fontillasA","fontillasT","fontillasMP",
+        "hidalgoEA","hidalgoET","hidalgoA","hidalgoT","hidalgoMP",
+        "jugnoEA","jugnoET","jugnoA","jugnoT","jugnoMP",
+        "lanoEA","lanoET","lanoA","lanoT","lanoMP",
+        "leyesaEA","leyesaET","leyesaA","leyesaT","leyesaMP",
+        "lorenteEA","lorenteET","lorenteA","lorenteT","lorenteMP",
+        "lubisEA","lubisET","lubisA","lubisT","lubisMP",
+        "magnoEA","magnoET","magnoA","magnoT","magnoMP",
+        "magsumbolEA","magsumbolET","magsumbolA","magsumbolT","magsumbolMP",
+        "recintoEA","recintoET","recintoA","recintoT","recintoMP",
+        "vergaraLEA","vergaraLET","vergaraLA","vergaraLT","vergaraLMP",
+        "andalEA","andalET","andalA","andalT","andalMP",
+        "bermilloEA","bermilloET","bermilloA","bermilloT","bermilloMP",
+        "capinigEA","capinigET","capinigA","capinigT","capinigMP",
+        "catapangHZEA","catapangHZET","catapangHZA","catapangHZT","catapangHZMP",
+        "delavegaEA","delavegaET","delavegaA","delavegaT","delavegaMP",
+        "llaveEA","llaveET","llaveA","llaveT","llaveMP",
+        "malonesEA","malonesET","malonesA","malonesT","malonesMP",
+        "nazEA","nazET","nazA","nazT","nazMP",
+        "pesaEA","pesaET","pesaA","pesaT","pesaMP",
+        "ramosEA","ramosET","ramosA","ramosT","ramosMP",
+        "roblesEA","roblesET","roblesA","roblesT","roblesMP",
+        "tiquisEA","tiquisET","tiquisA","tiquisT","tiquisMP",
+        "vergaraTMEA","vergaraTMET","vergaraTMA","vergaraTMT","vergaraTMMP"
+    ];
+
+    ids.forEach(id => {
+        document.getElementById(id).innerHTML = "0";
+    });
+}
+
 function june() {
+  clearAttendance();
 document.getElementById("magnoA").innerHTML = "1";
   document.getElementById("malonesA").innerHTML = "1";
   document.getElementById("magnoT").innerHTML = "1";
@@ -48,6 +92,7 @@ document.getElementById("magnoA").innerHTML = "1";
 }
 
 function july() {
+  clearAttendance();
   document.getElementById("leyesaA").innerHTML = "2";
    document.getElementById("leyesaT").innerHTML = "1";
    document.getElementById("hidalgoT").innerHTML = "1";
@@ -63,7 +108,7 @@ function july() {
 }
 
 function august() {
-
+clearAttendance();
 document.getElementById("deleonA").innerHTML = "1";
 document.getElementById("catapangJMEA").innerHTML = "1";
 document.getElementById("boncalosA").innerHTML = "1";
